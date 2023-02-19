8f39cb422cf7835d66c67e1df5c33e845b80c96d
halgord-darbandi <halgord.jwanro@gmail.com>
2023-02-19 08:23:37 -0800

    family En provider

--- src/Providers/En/FamilyProvider.php
+++ src/Providers/En/FamilyProvider.php
@@ -10,6 +10,14 @@ class FamilyProvider implements Generatable{
     use GenerateTrait;
 
     protected array $storage = [
-
+        'darbandi','mohammadi','moradi','momeni','karimi','askari','saedi','khalighi','karegar','mostafaye',
+        'safari','shahmari','kord','kordi','khezri','khezr pur','nader pur','naderi','mahmudi','ghanbari',
+        'ghaderi','saberi','sadeghi','jaberi','jabari','abdulahi','nosrati','narimani','zeyghami','khodadad',
+        'khodadadi','khodamoradi','derakhshan','derakhshani','daraye','yari','shafiye','nokhasi','azimi','ezati',
+        'salehi','besharati','zangeneh','rejali','mardani','rostami','weysi','azimi','ebrahimi','esmaeli','salimi',
+        'kamrani','kamrava','esfehani','tehrani','kalantari','lashkari','rezaye','ali mohammadi','ahmadi','sufi',
+        'sabagh','karim nezhad','karami khah','saburi','mehdi pur','irani','iran nezhad','nazeri','yusefi',
+        'jamali','jamalzadeh','kamali','kamalzadeh','yasini','lohrasbi','hushangi','walizadeh','tawakoli',
+        'tawasoli','bayat','taremi','shojaye','karim zadeh'
     ];
 }
