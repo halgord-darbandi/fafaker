441d6204eb91afd89b58190164d6b08f8e7fbe72
halgord-darbandi <halgord.jwanro@gmail.com>
2023-02-19 08:10:02 -0800

    family provider bug fix

--- src/Providers/Contracts/GenerateTrait.php
+++ src/Providers/Contracts/GenerateTrait.php
@@ -17,6 +17,16 @@ trait GenerateTrait{
      * randomly return data from providers storages;
      */
     public static function getFromStorage(){
+        if (self::isAssocArray(self::$storage)){
+            return self::$storage[array_rand(self::$storage)][rand(0,sizeof(self::$storage[array_rand(self::$storage)]))];
+        }
         return self::$storage[array_rand(self::$storage)];
+    }
+
+    /*
+     * checks is array is associative
+    */
+    public static function isAssocArray($array):bool{
+        return (is_string(array_key_first($array)) && array_key_first($array) !== 0);
     }
 }
